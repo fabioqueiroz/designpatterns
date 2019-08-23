b4d5016a5ded39bfd5f0341eb9e3ebfae5f04cc6
fabio <fabiovianaqueiroz@gmail.com>
2019-08-23 19:26:35 +0100

    memento

---
+++ Memento/ProspectMemory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    /// <summary>
+
+    /// The 'Caretaker' class
+
+    /// </summary>
+
+    public class ProspectMemory
+
+    {
+
+        private Memento _memento;
+
+
+
+        // Property
+
+        public Memento Memento
+
+        {
+
+            set { _memento = value; }
+
+            get { return _memento; }
+
+        }
+
+    }
+}
